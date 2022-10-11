b3cfa36475508ad7581670176c6f6e3cb67b4270
SkylerWRLD <115524536+SkylerWRLD@users.noreply.github.com>
2022-10-11 17:09:13 -0500

    Update CopyLink.lua

--- CopyLink.lua
+++ CopyLink.lua
@@ -3,7 +3,7 @@ local PremiumSection = SpectrumUI:WaitForChild("Main"):WaitForChild("Sections"):
 
 PremiumSection:WaitForChild("Bottom"):WaitForChild("Link"):WaitForChild("Button").MouseButton1Click:Connect(function()
 	setclipboard("https://spectrumhub.site/purchase")
-	PremiumSection:WaitForChild("Bottom"):WaitForChild("Link"):WaitForChild("Button"):WaitForChild("Label").TextSize = 15
+	PremiumSection:WaitForChild("Bottom"):WaitForChild("Link"):WaitForChild("Button"):WaitForChild("Label").TextSize = 18
 	PremiumSection:WaitForChild("Bottom"):WaitForChild("Link"):WaitForChild("Button"):WaitForChild("Label").Text = "Copied Purchase Link"
 	wait(2)
 	PremiumSection:WaitForChild("Bottom"):WaitForChild("Link"):WaitForChild("Button"):WaitForChild("Label").TextSize = 20
