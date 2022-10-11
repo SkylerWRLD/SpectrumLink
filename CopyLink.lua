43af8e3e445399c46f981c21acca49e9b53e073e
SkylerWRLD <115524536+SkylerWRLD@users.noreply.github.com>
2022-10-11 17:00:36 -0500

    Create CopyLink.lua

---
+++ CopyLink.lua
@@ -0,0 +1,11 @@
+local SpectrumUI = game.Players.LocalPlayer.PlayerGui:WaitForChild("Spectrum")
+local PremiumSection = SpectrumUI:WaitForChild("Main"):WaitForChild("Sections"):WaitForChild("PremiumSection")
+
+PremiumSection:WaitForChild("Bottom"):WaitForChild("Link"):WaitForChild("Button").MouseButton1Click:Connect(function()
+	setclipboard("https://spectrumhub.site/purchase")
+	PremiumSection.Bottom.Link.Button.Label.TextScaled = true
+	PremiumSection.Bottom.Link.Button.Label.Text = "Copied The Purchase Link"
+	wait(2)
+	PremiumSection.Bottom.Link.Button.Label.TextScaled = false
+	PremiumSection.Bottom.Link.Button.Label.Text = "Copy Purchase Link"
+end)
